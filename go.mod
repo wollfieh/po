aeb14e618eb102b2d0a0d1dcfb7c08d6395a07b0
Wolfgang Braun <wolfgang.braun@gmx.de>
2024-01-26 15:23:57 +0100

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module pushover
+
+go 1.21.6
+
+require github.com/gregdel/pushover v1.3.0
---
+++ pushover.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/gregdel/pushover"
+)
+
+func main() {
+
+	if len(os.Args) < 2 {
+		fmt.Printf("%s <messsage>\n", os.Args[0])
+
+	}
+	userKey := "akwjcknmaxrtx4sven9x2oy4974pm7"
+	apiToken := "u9ps3pf4e72o39dksnhwcztd287x3b"
+	// Create a new pushover app with a token
+	app := pushover.New(userKey)
+
+	// Create a new recipient
+	recipient := pushover.NewRecipient(apiToken)
+
+	// Create the message to send
+	message := pushover.NewMessage(strings.Join(os.Args[1:], " "))
+
+	// Send the message to the recipient
+	response, err := app.SendMessage(message, recipient)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	// Print the response if you want
+	log.Println(response)
+}
